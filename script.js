71520c09b328ebd80fef85adb4a3aa59e5312032
Marcos Manzanares <marcosmanzanarescampo@gmail.com>
2025-03-15 17:59:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const initPage = () => {
+    const dropDownElements = document.querySelectorAll(".dropdown");
+
+    dropDownElements.forEach(elem => {
+        elem.children[0].addEventListener('click', (event) => {    
+            event.target.parentElement.children[1].classList.toggle("show");
+        })
+    })
+}
+
+initPage();
