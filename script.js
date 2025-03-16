73f08bf0ba20ba1aa0d999ee768ebb8f202730c0
marcosmanzanarescampo <marcosmanzanarescampo@gmail.com>
2025-03-16 07:48:17 +0100

    deploiment submenu

---
+++ script.js
@@ -0,0 +1,14 @@
+const initPage = () => {
+    const dropDownElements = document.querySelectorAll(".dropdown");
+
+    dropDownElements.forEach(elem => {
+        elem.children[0].addEventListener('click', (event) => {    
+            event.target.parentElement.children[1].classList.toggle("show");
+            event.target.classList.toggle("active");
+            event.target.style.color = "white";
+            
+        })
+    })
+}
+
+initPage();
